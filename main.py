c5f5f218fdd89f2b9dbce802330a05ab8aa6b280
Chittesh-Kumar <128819733+ChitteshK139@users.noreply.github.com>
2024-01-04 11:48:45 +0530

    training added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Asian_African_Elephants_Classification import logger
 from Asian_African_Elephants_Classification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from Asian_African_Elephants_Classification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from Asian_African_Elephants_Classification.pipeline.stage_03_training import ModelTrainingPipeline
 
 STAGE_NAME= "Data Ingestion Stage"
 
@@ -19,6 +20,19 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_ingestion =PrepareBaseModelTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Training"
+
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainingPipeline()
+    obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     
 except Exception as e:
